bc044aff004f34d1bbe7acb125eefdefa628063d
edududu93 <134012393+edududu93@users.noreply.github.com>
2025-02-07 20:47:15 +0000

    encode/decode test

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -11,8 +11,8 @@ func TestConnect(t *testing.T) {
 
 	tra.Connect(trb)
 	trb.Connect(tra)
-	assert.Equal(t, tra.peers[trb.addr], trb)
-	assert.Equal(t, trb.peers[tra.addr], tra)
+	assert.Equal(t, tra.peers[trb.Addr()], trb)
+	assert.Equal(t, trb.peers[tra.Addr()], tra)
 }
 
 func TestSendMessage(t *testing.T) {
@@ -29,3 +29,4 @@ func TestSendMessage(t *testing.T) {
 	assert.Equal(t, rpc.Payload, msg)
 	assert.Equal(t, rpc.From, tra.addr)
 }
+
--- network/transport.go
+++ network/transport.go
@@ -1,17 +1,16 @@
 package network
 
-//import "fmt"
-
 type NetAddr string
 
 type RPC struct {
-	From NetAddr
+	From 	NetAddr
 	Payload []byte
 }
 
 type Transport interface {
-	Consume() <-chan RPC // YT: Consume <-chan RPC ### CHAT: Consume() <-chan RPC
+	Consume() <-chan RPC
 	Connect(Transport) error
-	SendMessage(NetAddr, []byte) error // YT: SendMessage(NetAddr, payload []byte) error ### CHAT: SendMessage(to NetAddr, []byte) error
+	SendMessage(NetAddr, []byte) error
 	Addr() NetAddr
 }
+
