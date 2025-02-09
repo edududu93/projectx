b0cf1f88433c8e7c7ab0f0aeb6620805789a0f02
edududu93 <134012393+edududu93@users.noreply.github.com>
2025-02-09 21:51:33 +0000

    Block Hasher

--- core/block_test.go
+++ core/block_test.go
@@ -1,65 +1,27 @@
 package core
 
 import (
-	"bytes"
 	"testing"
 	"time"
 	"fmt"
 
 	"projectx/types"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestHeader_Encode_Decode(t *testing.T) {
-	h := &Header{
-		Version:   1,
-		PrevBlock: types.RandomHash(),
-		Timestamp: time.Now().UnixNano(),
-		Height:    10,
-		Nonce:     989394,
+func randomBlock(height uint32) *Block {
+	header := &Header{
+		Version:		1,
+		PrevBlockHash:  types.RandomHash(),
+		Height: 		height,
+		Timestamp: 		time.Now().UnixNano(),
 	}
-
-	buf := &bytes.Buffer{}
-	assert.Nil(t, h.EncodeBinary(buf))
-
-	hDecode := &Header{}
-	assert.Nil(t, hDecode.DecodeBinary(buf))
-	assert.Equal(t, h, hDecode)
+	tx := Transaction{
+		Data: []byte("foo"),
+	}
+	return NewBlock(header, []Transaction{tx})
 }
 
-func TestBlock_Encode_Decode(t *testing.T) {
-	b := &Block{
-		Header: Header{
-			Version:   1,
-			PrevBlock: types.RandomHash(),
-			Timestamp: time.Now().UnixNano(),
-			Height:    10,
-			Nonce:     989394,
-		},
-		Transactions: nil,
-	}
-
-	buf := &bytes.Buffer{}
-	assert.Nil(t, b.EncodeBinary(buf))
-
-	bDecode := &Block{}
-	assert.Nil(t, bDecode.DecodeBinary(buf))
-	assert.Equal(t, b, bDecode)
-	fmt.Printf("%+v", bDecode)
-}
-
-func TestBlockhash (t *testing.T) {
-	b := &Block {
-		Header: Header{
-			Version:   1,
-			PrevBlock: types.RandomHash(),
-			Timestamp: time.Now().UnixNano(),
-			Height:    10,
-			Nonce:     989394,
-		},
-		Transactions: []Transaction{},
-	}
-	h := b.Hash()
-	fmt.Println(h)
-	assert.False(t, h.IsZero())
+func TestHashBlock(t *testing.T) {
+	b := randomBlock(0)
+	fmt.Println(b.Hash(BlockHasher{}))
 }
--- core/transaction.go
+++ core/transaction.go
@@ -1,11 +1,12 @@
 package core
 
-import "io"
+import (
+	"projectx/crypto"
+)
 
 type Transaction struct {
 	Data []byte 
+
+	PublicKey crypto.PublicKey
+	Signature *crypto.Signature
 }
-
-func (tx *Transaction) DecodeBinary(r io.Reader) error {return nil}
-
-func (tx *Transaction) EncodeBinary(w io.Writer) error {return nil}
